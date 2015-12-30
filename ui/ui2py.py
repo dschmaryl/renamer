81afaa17738680548ec9febc511ae9d60fb3e9fe
dschmaryl <dschmaryl@gmail.com>
2015-12-29 19:31:24 -0500

    port ui2py.py to python3

--- ui/ui2py.py
+++ ui/ui2py.py
@@ -1,9 +1,10 @@
-from os import listdir, system
-from os.path import isfile
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-for f in listdir('./'):
-    if isfile(f) and '.ui' in f[-3:]:
-        ui_name = f
-        py_name = f[:-3] + '.py'
-        cmd = 'pyside-uic ' + ui_name + ' -o ' + py_name
-        system(cmd)
+import glob
+import subprocess
+
+for ui_file in glob.glob('*.ui'):
+    py_file = ui_file.replace('.ui', '.py')
+    cmd = ['pyside-uic', ui_file, '-o', py_file]
+    subprocess.run(cmd)
